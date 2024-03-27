a81ab192bfd8d1670296ef4c654320d73a37882b
Shivdatta redekar <shivdattaredekar@gmail.com>
2024-03-27 15:43:12 +0530

    looging, exception and utlis creation done

---
+++ main.py
@@ -0,0 +1,3 @@
+from TextSummarizer.logging import logger
+
+logger.info('Welcome to our custom logs')
---
+++ src/TextSummarizer/utils/common.py
@@ -0,0 +1,71 @@
+# utlis is the file thatis being used to store the function that are going to be used again and again. hence insted 
+    #of calling them again and again we can store them here in separate folder called common so that they can be 
+    #called via packages
+
+import os
+from box.exceptions import BoxValueError #we will create any separate exception insted we will use this
+import yaml
+from TextSummarizer.logging import logger
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+
+
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """reads yaml file and returns
+
+    Args:
+        path_to_yaml (str): path like input
+
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+
+    Returns:
+        ConfigBox: ConfigBox type
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+    
+
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
+
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
+
+
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """get size in KB
+
+    Args:
+        path (Path): path of the file
+
+    Returns:
+        str: size in KB
+    """
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~ {size_in_kb} KB"
+
+    
+ 
